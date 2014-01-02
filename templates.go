76cb6c209f2a9817839630dc82e9bca2a1281b76
Craig Weber <weberc2@gmail.com>
2014-01-01 22:41:59 -0600

    Added Google Analytics

--- templates.go
+++ templates.go
@@ -60,6 +60,16 @@ h1, h2 {
 		<style>
 			%s
 		</style>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-46738088-1', 'weberc2.com');
+		  ga('send', 'pageview');
+
+		</script>
 	</head>
 	<body>
 		<div class="main">
