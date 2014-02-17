01644cc215cb63ad0583fbeb43123b60e5b2af20
weberc2 <weberc2@gmail.com>
2014-02-17 09:16:54 -0600

    Update parser.go
    Updated documentation

--- parser.go
+++ parser.go
@@ -26,6 +26,15 @@ func parseMetadata(line string) *metadata {
 	}
 }
 
+// Title: A metadata document parser
+// Date: 2/17/14
+// # A metadata document parser
+//
+// Parses a metadata document. This function will iterate over each line in the
+// reader--as long as the lines are in `key: value` format, the parser will
+// interpret the lines as metadata; however, once it finds a line that isn't in
+// that format, that line and all subsequent lines will be interpreted as the
+// document body. This comment is an example of such a document.
 func (p *MetadataParser) Parse(r io.Reader) (*Document, error) {
 	s := bufio.NewScanner(r)
 	d := &Document{ Metadata: make(map[string]string) }
